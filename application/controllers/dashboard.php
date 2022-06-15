8b32f8f8c581cbc7d4d70671b50e64f9aae52146
RusithT <rusithtl99@gmail.com>
2022-06-15 22:43:03 +0530

    home

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -3,6 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
 	public function index(){
-		echo "this is dashboard"
+		$this->load->view('home');
 	}
 }
