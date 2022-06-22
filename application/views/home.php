a87c16a93725a6cd6b1972c27c92533c5e1cc94a
Tharini2000 <tharinithisakya.hw@gmail.com>
2022-06-22 22:07:34 +0530

    Create login page

--- application/views/home.php
+++ application/views/home.php
@@ -92,7 +92,7 @@
                 <a class="nav-link" href="<?= base_url(); ?>contact">Contact Us</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="contact.html">Login</a>
+                <a class="nav-link" href="<?php echo base_url() ?>index.php/login">Login</a>
               </li>
               <li class="nac-item">
                 <a class="nav-link" href="<?php echo base_url() ?>index.php/register">Register</a>
