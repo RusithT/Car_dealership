1cbd41333333de9394416304e373fbc08e3e7506
chathuraP4 <chathurangaliyanage2018@gmail.com>
2022-06-22 23:06:10 +0530

    update1

--- application/controllers/Apply_lease.php
+++ application/controllers/Apply_lease.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class apply_lease extends CI_Controller {
+class Apply_lease extends CI_Controller {
 	public function index(){
 		$this->load->view('header');
 		$this->load->view('apply_lease');
