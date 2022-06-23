3b4134216a95a5c11c7cc9d8771b1a5c49c40b57
chathuraP4 <chathurangaliyanage2018@gmail.com>
2022-06-23 23:15:55 +0530

    footer some changes

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class Register extends CI_Controller {
 	public function index(){
 		$this->load->view('header');
 		$this->load->view('register');
