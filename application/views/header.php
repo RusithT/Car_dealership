32f4b2d2b45319b4b150f16f73b4b83cf567cf26
Sajinth Fernando <sajinth12fernando@gmail.com>
2022-06-24 08:22:39 +0530

    parts page updated

--- application/views/header.php
+++ application/views/header.php
@@ -84,6 +84,7 @@
                     <a class="dropdown-item" href="about.html">About Us</a>
                     <a class="dropdown-item" href="blog.html">Auction</a>
                     <a class="dropdown-item" href="<?php echo base_url() ?>index.php/lease">Lease</a>
+                    <a class="dropdown-item" href="<?php echo base_url() ?>index.php/parts">Spare Parts</a>
                     <a class="dropdown-item" href="team.html">Team</a>
                     <a class="dropdown-item" href="testimonials.html">Blog</a>
                     <a class="dropdown-item" href="faq.html">FAQ</a>
