125626580314d56691ede346bae762e364b394d3
YohaniJ <yohanijalill12@gmail.com>
2022-06-26 14:18:30 +0530

    exchange page

--- application/controllers/exchange.php
+++ application/controllers/exchange.php
@@ -5,6 +5,7 @@ class Exchange extends CI_Controller {
 	public function index(){
 		$this->load->view('header');
 		$this->load->view('exchange');
-		$this->load->view('footer');
+        $this->load->view('footer');
+		
 	}
 }
