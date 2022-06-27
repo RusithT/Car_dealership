2984455539cb8cf847486675a1a1b3378ea799e6
Tharini2000 <tharinithisakya.hw@gmail.com>
2022-06-27 12:45:35 +0530

    login buttons change

---
+++ application/views/login.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/bootstrap.min.css">
+    
+    <!-- Style -->
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/tstyle.css">
+
+    <title>Login</title>
+  </head>
+  <body>
+  
+
+  <div class="d-lg-flex half">
+    <div class="bg order-1 order-md-1" style="background-image: url('assets/images/tcars.jpg');"></div>
+    <div class="contents order-2 order-md-1">
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-7">
+            <div class="mb-4">
+              <h5><b>Login with your credentials</b></h5>
+            </div>
+
+            <form action="#" method="post">
+              <div class="form-group first">
+                <label for="username">E-mail</label>
+                <input type="text" class="form-control" id="e-mail">
+              </div>
+
+              <div class="form-group last mb-3">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password">
+              </div>
+              
+              <div class="d-flex mb-5 align-items-center">
+                <label class="control control--checkbox mb-0"><span class="caption"><b>Remember me</b></span></span>
+                  <input type="checkbox" checked="checked"/>
+                  <div class="control__indicator"></div>
+                </label>
+                <span class="ml-auto"><a href="#" class="forgot-pass"><b>Forgot Password</b></a></span> 
+              </div>
+
+              <input type="submit" value="LOGIN" class="btn btn-block btn-primary">
+
+              <span class="d-block text-center my-4 text-muted">&mdash; or &mdash;</span>
+              
+              <div class="social-login">
+                <a href="#" class="facebook btn d-flex justify-content-left align-items-left">
+                  <span class="icon-facebook mr-3"></span> Continue with Facebook
+                </a>
+                <a href="#" class="google btn d-flex justify-content-center align-items-center">
+                  <span class="icon-google mr-3"></span> Continue with  Google
+                </a>
+                <a href="#" class="Apple btn d-flex justify-content-center align-items-center">
+                  <span class="icon-apple mr-3"></span> Continue with  Apple
+                </a> 
+              </div><br>
+              <div class="container Register">
+                <p><b>Don't have any account?</b> <a href="#"><b>Register Here</b></a>.</p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+  </div>
+    
+    
+
+    <script src="<?= base_url(); ?>assets/js/jquery-3.3.1.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/popper.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/bootstrap.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/tmain.js"></script>
+  </body>
+</html>
