5f2355222a24c8fade2401368332197172cf45df
KalinduW <kalinduhansaka07@gmail.com>
2022-06-28 09:49:36 +0530

    Update Vehicle.php

--- application/controllers/Vehicle.php
+++ application/controllers/Vehicle.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Vehicle extends CI_Controller {
 	public function index(){
+		$this->load->view('header');
 		$this->load->view('vehicle');
 		$this->load->view('footer');
 	}
