1dcb421351e9372a7cfabe4290e8f30358755bb3
chathuraP4 <chathurangaliyanage2018@gmail.com>
2022-07-17 22:29:52 +0530

    merge lease calculator

--- application/models/lease_model.php
+++ application/models/lease_model.php
@@ -1,11 +1,13 @@
 <?php
-class Compare_model extends CI_Model {
+class Lease_model extends CI_Model
+{
 
-	public function __construct(){
-		$this->load->database();
-	}
+    public function __construct()
+    {
+        $this->load->database();
+    }
 
-	public function select_type(){
+    public function select_type(){
 
         $sql="SELECT * FROM `vehicle_type`";
         $query = $this->db->query($sql)->result();
@@ -44,117 +46,90 @@ class Compare_model extends CI_Model {
 		
 	}
 
+    public function load_item(){
 
-    public function select_type2(){
-
-        $sql="SELECT * FROM `vehicle_type` WHERE id='".$_POST['type']."'";
+        $sql="SELECT * FROM `vehicles` WHERE vehicle_id='".$_POST['type1']."' AND brand_id='".$_POST['brand1']."' AND model_id='".$_POST['model1']."' ";
         $query = $this->db->query($sql)->result();
         
-        $option="<option class='dropdown-item option1' value='0' style='color:white;' >SELECT TYPE</option>";
+        $option="<option class='dropdown-item option1' value='0' style='color:white;' >SELECT Item</option>";
         foreach($query as $row){
-            $option.="<option class='dropdown-item' value='".$row->id."'>".$row->type."</option>"; 
+            $option.="<option class='dropdown-item' value='".$row->chesis_no."'>".$row->chesis_no." - ". $row->manufacturer_id."</option>"; 
         }
         return ($option);
 		
 	}
+    public function lease_rate(){
 
+         $sql1="SELECT * FROM `lease_rate` WHERE v_type='".$_POST['type1']."' AND v_brand='".$_POST['brand1']."' AND v_model='".$_POST['model1']."'AND reg_status='".$_POST['status']."' AND conditions='".$_POST['condition']."' ";
+         $query = $this->db->query($sql1)->first_row()->rate;
+        
+       
+         return ($query);
+		
+	}
+    public function full_ammount(){
 
+        $sql1="SELECT * FROM `vehicles` WHERE vehicle_id='".$_POST['type1']."' AND brand_id='".$_POST['brand1']."' AND model_id='".$_POST['model1']."'AND chesis_no='".$_POST['chesis_no']."' ";
+        $query = $this->db->query($sql1)->row()->selling_price;
+       
+      
+        return ($query);
+       
+   }
+    public function lease_vehicles(){
 
-
-	public function compare_vehicles(){
-
-        $sql1="SELECT * FROM `vehicles` WHERE vehicle_id='".$_POST['type1']."' AND brand_id='".$_POST['brand1']."' AND model_id='".$_POST['model1']."'";
+        $sql1="SELECT * FROM `vehicles` WHERE vehicle_id='".$_POST['type1']."' AND brand_id='".$_POST['brand1']."' AND model_id='".$_POST['model1']."'AND chesis_no='".$_POST['chesis_no']."' ";
         $row1 = $this->db->query($sql1)->row();
-        
-        $sql2="SELECT * FROM `vehicles` WHERE vehicle_id='".$_POST['type2']."' AND brand_id='".$_POST['brand2']."' AND model_id='".$_POST['model2']."'";
-        $row2 = $this->db->query($sql2)->row();
-        
         
         $option="<table class='table table-bordered' style='position:relative;'>";
         $option.="<thead class='thead-dark'>";
         $option.="<tr>";
         $option.="<th style='width: 40%;'>OVERVIEW</th>";
         $option.="<th style='width: 30%;'>Vehicle 1</th>";
-        $option.="<th style='width: 30%;'>Vehicle 2</th>";
         $option.="</tr>";
         $option.="</thead>";
      //   foreach($query as $row){
             $option.="<tr>";
-            $option.="<td>Category</td>";
-            $option.="<td>".$row1->category."</td>";
-            $option.="<td>".$row2->category."</td>";
-            $option.="</tr>";
-
-            $option.="<tr>";
-            $option.="<td>Buying Price</td>";
-            $option.="<td>".$row1->buying_price."</td>";
-            $option.="<td>".$row2->buying_price."</td>";
-            $option.="</tr>";
-
-            $option.="<tr>";
             $option.="<td>Selling Price</td>";
             $option.="<td>".$row1->selling_price."</td>";
-            $option.="<td>".$row2->selling_price."</td>";
             $option.="</tr>";
 
             $option.="<tr>";
             $option.="<td>Milage</td>";
             $option.="<td>".$row1->mileage."</td>";
-            $option.="<td>".$row2->mileage."</td>";
             $option.="</tr>";
 
             $option.="<tr>";
             $option.="<td>Color</td>";
             $option.="<td>".$row1->color."</td>";
-            $option.="<td>".$row2->color."</td>";
             $option.="</tr>";
 
             $option.="<tr>";
             $option.="<td>Registration Year</td>";
             $option.="<td>".$row1->registration_year."</td>";
-            $option.="<td>".$row2->registration_year."</td>";
             $option.="</tr>";
 
             $option.="<tr>";
             $option.="<td>Gear</td>";
             $option.="<td>".$row1->gear."</td>";
-            $option.="<td>".$row2->gear."</td>";
             $option.="</tr>";
 
             $option.="<tr>";
             $option.="<td>Doors</td>";
             $option.="<td>".$row1->doors."</td>";
-            $option.="<td>".$row2->doors."</td>";
             $option.="</tr>";
 
             $option.="<tr>";
             $option.="<td>Seats</td>";
             $option.="<td>".$row1->seats."</td>";
-            $option.="<td>".$row2->seats."</td>";
             $option.="</tr>";
             
             $option.="<tr>";
             $option.="<td>Tank</td>";
             $option.="<td>".$row1->tank."</td>";
-            $option.="<td>".$row2->tank."</td>";
             $option.="</tr>";
-
-            $option.="<tr>";
-            $option.="<td>Engine No</td>";
-            $option.="<td>".$row1->engine_no."</td>";
-            $option.="<td>".$row2->engine_no."</td>";
-            $option.="</tr>";
-
-            $option.="<tr>";
-            $option.="<td>Chassis No</td>";
-            $option.="<td>".$row1->chesis_no."</td>";
-            $option.="<td>".$row2->chesis_no."</td>";
-            $option.="</tr>";
-
-
      //   }
         return ($option);
 		
 	}
-	
 }
--- application/views/lease.php
+++ application/views/lease.php
@@ -1,51 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
+<head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
-    <title>BROOOOM.LK</title>
+  <title>BROOOOM.LK</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="<?= base_url(); ?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap core CSS -->
+  <link href="<?= base_url(); ?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/fontawesome.css">
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css">
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/owl.css">
-  </head>
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" href="<?= base_url(); ?>assets/css/fontawesome.css">
+  <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css">
+  <link rel="stylesheet" href="<?= base_url(); ?>assets/css/owl.css">
+</head>
 
 <body>
-    <div class="sub-header">
-        <div class="container">
-            <div class="row">
-            <div class="col-md-8 col-xs-12">
-                <ul class="left-info">
+  <div class="sub-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-xs-12">
+          <ul class="left-info">
 
-                <li><a href="#"><i class="fa fa-envelope"></i>broooom.lk@gmail.com</a></li>
-                <li><a href="#"><i class="fa fa-phone"></i>0711231545</a></li>
-            </div>
-            <div class="col-md-4">
-                <ul class="right-icons">
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                </ul>
-            </div>
-            </div>
+            <li><a href="#"><i class="fa fa-envelope"></i>broooom.lk@gmail.com</a></li>
+            <li><a href="#"><i class="fa fa-phone"></i>0711231545</a></li>
         </div>
+        <div class="col-md-4">
+          <ul class="right-icons">
+            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
     </div>
+  </div>
 
-    <header class="">
-        <div class="main-banner header-text" id="top">
+  <header class="">
+    <div class="main-banner header-text" id="top">
       <nav class="navbar navbar-expand-lg">
         <div class="container">
-          <a class="navbar-brand" href="<?= base_url(); ?>"><h2>BR0000M.LK</h2></a>
+          <a class="navbar-brand" href="<?= base_url(); ?>">
+            <h2>BR0000M.LK</h2>
+          </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -64,15 +66,15 @@
               </li>
               <li class="nav-item dropdown">
                 <a class="dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Other</a>
-              
+
                 <div class="dropdown-menu">
-                    <a class="dropdown-item" href="about.html">About Us</a>
-                    <a class="dropdown-item" href="blog.html">Auction</a>
-                    <a class="dropdown-item" href="team.html">Team</a>
-                    <a class="dropdown-item" href="testimonials.html">Blog</a>
-                    <a class="dropdown-item" href="faq.html">FAQ</a>
-                    <a class="dropdown-item" href="terms.html">Terms</a>
-                    <a class="dropdown-item" href="review.html">Review</a>
+                  <a class="dropdown-item" href="about.html">About Us</a>
+                  <a class="dropdown-item" href="blog.html">Auction</a>
+                  <a class="dropdown-item" href="team.html">Team</a>
+                  <a class="dropdown-item" href="testimonials.html">Blog</a>
+                  <a class="dropdown-item" href="faq.html">FAQ</a>
+                  <a class="dropdown-item" href="terms.html">Terms</a>
+                  <a class="dropdown-item" href="review.html">Review</a>
                 </div>
               </li>
               <li class="nav-item">
@@ -88,118 +90,50 @@
           </div>
         </div>
       </nav>
-    </header>
-    <br/><br/><br/></br>
-    <div class="row">
-      <div class="col-md-3">
-              <div class="service-item">
-                <img src="<?= base_url(); ?>assets/images/cal.jpg" alt="">
-                <div class="down-content">
-                  <h4>Calculate your lease here...</h4>
-                  <div style="margin-bottom:10px;">
-                    <span>from <sup>RS.</sup>4500 per weekend</span>
-                  </div>
-                  <p>You can calculate your lease by choosing easy way to do the payment.</p>
-                  <a href="#" data-toggle="modal" data-target="#exampleModal" class="filled-button">Try Now</a>
-                </div>
-              </div>
-        </div>
-      <div class="col-md-4">
-        <div class="service-item">
-          <img src="<?= base_url(); ?>assets/images/handshake.jpg" alt="">
-          <div class="down-content">
-            <h4>Want to apply for a lease?</h4>
-            <!-- <div style="margin-bottom:20px;">
-              <span>from <sup>RS.</sup>4500 per weekend</span>
-            </div> -->
-            <p>You can apply for a lese from here...</p>
-            <a href="<?php echo base_url() ?>terms" class="filled-button">Apply now</a>
+  </header>
+  <br /><br /><br /></br>
+  <div class="row">
+    <div class="col-md-3">
+      <div class="service-item">
+        <img src="<?= base_url(); ?>assets/images/cal.jpg" alt="">
+        <div class="down-content">
+          <h4>Calculate your lease here...</h4>
+          <div style="margin-bottom:10px;">
+            <span>from <sup>RS.</sup>4500 per weekend</span>
           </div>
+          <p>You can calculate your lease by choosing easy way to do the payment.</p>
+          <a href="<?php echo base_url() ?>lease_calculator"data-toggle="" data-target="" class="filled-button">Try Now</a>
         </div>
+      </div>
     </div>
     <div class="col-md-4">
-        <div class="service-item">
-          <img src="<?= base_url(); ?>assets/images/handshake.jpg" alt="">
-          <div class="down-content">
-            <h4>Want to apply for a lease?</h4>
-            <!-- <div style="margin-bottom:20px;">
+      <div class="service-item">
+        <img src="<?= base_url(); ?>assets/images/handshake.jpg" alt="">
+        <div class="down-content">
+          <h4>Want to apply for a lease?</h4>
+          <!-- <div style="margin-bottom:20px;">
               <span>from <sup>RS.</sup>4500 per weekend</span>
             </div> -->
-            <p>You can apply for a lese from here...</p>
-            <a href="<?php echo base_url() ?>apply_lease" class="filled-button">Apply now</a>
-          </div>
+          <p>You can apply for a lese from here...</p>
+          <a href="<?php echo base_url() ?>terms" class="filled-button">Apply now</a>
         </div>
       </div>
-    </div>   
-
-
-
-<!-- Calculator -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style="margin-top: 100px;">
-      <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">LEASE CALCULATOR</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <form action="#" id="contact">
-              <label>Select Vehicle Type</label><br>
-              <select name="vehicle_type" id="vehicle_type" class="col-md-8">
-                <option id="car">Car</option>
-                <option id="van">Van</option>
-                <option id="truck">Truck</option>
-                <option id="suv">SUV</option>
-              </select><br><br>
-
-              <input type="radio" id="condition" name="condition">&nbsp;Recondition</input>
-              <input type="radio" id="condition" name="condition">&nbsp;Brand New</input><br>
-              <input type="radio" id="condition" name="condition">&nbsp;Registered</input>&nbsp;
-              <input type="radio" id="condition" name="condition">&nbsp;Unregistered</input><br><br>
-
-              <label>Lease Amount</label><br>
-              <input type="number" id="lease_amount" name="lease_amount" class="col-md-8" value="Put your lease amount here" min="0"><br><br>
-
-              <label>Lease Period</label><br>
-              <select class="col-md-8" name="lease_period" id="lease_period">
-                <option value="6">6 Month</option>
-                <option value="12">12 Month</option>
-                <option value="18">18 Month</option>
-                <option value="24">24 Month</option>
-                <option value="30">30 Month</option>
-                <option value="36">36 Month</option>
-                <option value="42">42 Month</option>
-                <option value="48">48 Month</option>
-                <option value="54">54 Month</option>
-                <option value="60">60 Month</option>
-                <option value="66">66 Month</option>
-                <option value="72">72 Month</option>
-              </select><br/> <br/>             
-              <p>The Amount is: Rs.<span id="amount"></span></p>
-          </div>
-          <div class="modal-footer">
-            <input type="button" class="btn btn-secondary" value="Reset"></button>
-            <input type="button" class="btn btn-primary" onclick="multiplyBy()" value="calculate"></button>
-            
-          </div>
-        </div>
-      </div>
-
-      <script>
-        function multiplyBy(){
-          num1= document.getElementById('lease_amount').value;
-          num2= document.getElementById('lease_period').value;
-          document.getElementById('amount').innerHTML = num1 * num2;
-        }
-      </script>
     </div>
+    <div class="col-md-4">
+      <div class="service-item">
+        <img src="<?= base_url(); ?>assets/images/handshake.jpg" alt="">
+        <div class="down-content">
+          <h4>Want to apply for a lease?</h4>
+          <!-- <div style="margin-bottom:20px;">
+              <span>from <sup>RS.</sup>4500 per weekend</span>
+            </div> -->
+          <p>You can apply for a lese from here...</p>
+          <a href="<?php echo base_url() ?>apply_lease" class="filled-button">Apply now</a>
+        </div>
+      </div>
+    </div>
+  </div>
 
-    
 
 
-
-
-</body>
-</html>
+  
